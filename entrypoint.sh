9ac93bd003ac9a47775e9da9720fc8a8a2e2f968
Andreas Tiefenthaler <at@an-ti.eu>
2022-12-28 10:58:46 +0700

    fix: debug build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 # Expand the arguments into an array of strings. This is required because the GitHub action
 # provides all arguments concatenated as a single string.
 
+ls -lah
+
 ARGS=("$@")
 
 /bin/kaowao ${ARGS[*]}
