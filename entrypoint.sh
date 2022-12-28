ff0ffa3f422152557653ffa8fe4419c6e3dae75b
Andreas Tiefenthaler <at@an-ti.eu>
2022-12-28 11:05:46 +0700

    fix: remove debug from entry.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,5 @@
 # Expand the arguments into an array of strings. This is required because the GitHub action
 # provides all arguments concatenated as a single string.
 
-ls -lah
-
 ARGS=("$@")
-
 /bin/kaowao ${ARGS[*]}
